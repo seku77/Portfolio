babbe0bb98d0e6ba14d73e3bf03068d1b1595470
seku77 <bartek.sek@o2.pl>
2020-10-18 22:40:44 +0200

    Added scroll buttons functionality

--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@
     var $gallery = new SimpleLightbox('.projects a', {});
     $gallery.docClose = false;
 })();
+
+$(document).ready(function() {
+    $('#aboutBtn').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $("#secondImage").offset().top
+      }, 600);
+    });
+  });
+
+  $(document).ready(function() {
+    $('#projectsBtn').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $(".separator").offset().top
+      }, 600);
+    });
+  });
+
+  $(document).ready(function() {
+    $('#contactBtn').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $(".contact").offset().top
+      }, 600);
+    });
+  });
