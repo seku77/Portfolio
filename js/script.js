625a2c2b941161fb3cd5e54d5a824e9f1743536c
seku77 <bartek.sek@o2.pl>
2020-10-19 13:39:11 +0200

    Updated smooth scroll script

--- js/script.js
+++ js/script.js
@@ -3,26 +3,39 @@
     $gallery.docClose = false;
 })();
 
-$(document).ready(function() {
-    $('#aboutBtn').click(function(event) {
-      $('body, html').animate({
-        scrollTop: $("#secondImage").offset().top
-      }, 600);
-    });
-  });
-
-  $(document).ready(function() {
-    $('#projectsBtn').click(function(event) {
-      $('body, html').animate({
-        scrollTop: $(".separator").offset().top
-      }, 600);
-    });
-  });
-
-  $(document).ready(function() {
-    $('#contactBtn').click(function(event) {
-      $('body, html').animate({
-        scrollTop: $(".contact").offset().top
-      }, 600);
-    });
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
