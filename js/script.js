9058ea1d3c0f12c2045cdee8fc1eb9e88036cee5
seku77 <bartek.sek@o2.pl>
2020-10-21 11:48:17 +0200

    Fixed footer positioning

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ $('a[href*="#"]')
           var $target = $(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
+            return false;       
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
